b7c8de30ced952b6f48dd814bc17071954982ff8
Bernd Herzog <e70fd4cf998e954b12e23bc983767992e1f4b9f6@nachregenkommtsonne.de>
2021-06-18 09:51:09 +0200

    implemented grpc server

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
+from concurrent import futures
+
+import grpc
+
+import yadc_pb2
+import yadc_pb2_grpc
+
+
 print("Hello World from yadc!")
+
+
+class YetAnotherDocumentConverter(yadc_pb2_grpc.YetAnotherDocumentConverterServicer):
+
+    def Convert(self, request, context):
+        return yadc_pb2.ConvertReply(ouputData=[42,21])
+
+
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+yadc_pb2_grpc.add_YetAnotherDocumentConverterServicer_to_server(YetAnotherDocumentConverter(), server)
+server.add_insecure_port('[::]:50051')
+server.start()
+server.wait_for_termination()
